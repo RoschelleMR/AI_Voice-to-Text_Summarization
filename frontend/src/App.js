704ce4a7c48734a9d5d5798bc9b60f53758baa16
Kareem Ellis <kareemellis55@gmail.com>
2023-11-23 00:23:36 -0500

    Updated frontend code to accommodate file uploads and error handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import AudioInput from './components/AudioInput';
-import TextDisplay from './components/TextDisplay';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import AudioInput from './components/AudioInput'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [isFileTranscribed, setIsFileTranscribed] = useState(false)
+  const [transcriptionData, setTranscriptionData] = useState({
+    summary: '',
+    transcription: ''
+  })
 
   //Just a function to test a Hello Worl route
   const testAPI = (e) => {
@@ -26,29 +28,35 @@ function App() {
     <div>
       <Header />
 
-      <button 
-        class="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition"
-        onClick={testAPI}
-      >
-        Test API
-      </button>
-
       <main class="flex-grow container mx-auto p-4 space-y-4">
-        <AudioInput setIsFileTranscribed={setIsFileTranscribed}/>
-        
+
+        <button 
+          class="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition"
+          onClick={testAPI}
+        >
+          Test API
+        </button>
+
+        <AudioInput setTranscriptionData={setTranscriptionData} />
+
+        {console.log(transcriptionData, "agaga")}
         {
           // Output Section (Hidden if there isn't a file that is currently transcribed)
-          isFileTranscribed &&
+          transcriptionData.transcription &&
           <section>
             <h2>Summarized Transcription</h2>
-            <TextDisplay />
+
+            <div id="summaryContainer" class="p-4 bg-white rounded shadow">
+              {transcriptionData.transcription}
+            </div>
+
             <button class="mt-3 px-4 py-2 bg-purple-600 text-white rounded hover:bg-green-700 transition">View Transcription</button>
           </section>
         }
 
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- frontend/src/components/AudioInput.js
+++ frontend/src/components/AudioInput.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import FailAlert from './FailAlert'
+import SuccessAlert from './SuccessAlert'
 
-export default function AudioInput() {
+export default function AudioInput({ setTranscriptionData }) {
   const [file, setFile] = useState(null)
   const [fileName, setFileName] = useState('')
+  const [fileTypeAlertActive, setFileTypeAlertActive] = useState(false)
+  const [uploadFailureAlertActive, setUploadFailureAlertActive] = useState(false)
+  const [uploadSuccessAlertActive, setUploadSuccessAlertActive] = useState(false)
 
   const onFormSubmit = (e) => {
     e.preventDefault()
+    setFileTypeAlertActive(false)
+    setUploadFailureAlertActive(false)
+    setUploadSuccessAlertActive(false)
     
     if (!file) {
       alert('Please select a file to upload!')
@@ -23,50 +31,71 @@ export default function AudioInput() {
       }
     })
     .then(response => {
-      alert("Uploaded Successfully")
+      setUploadSuccessAlertActive(true)
       console.log("File uploaded successfully", response.data);
+      setTranscriptionData(response.data)
       // Handle success
     })
     .catch(error => {
-      alert("Error uploading file")
-      console.error("Error uploading file", error);
+      setUploadFailureAlertActive(true)
+      console.error("Error uploading file", error.request.response);
       // Handle error
     });
   }
 
   const onFileChange = (e) => {
-    const selectedFile = e.target.files[0];
+    const selectedFile = e.target.files[0]
+    setUploadFailureAlertActive(false)
+    setUploadSuccessAlertActive(false)
+
     if (selectedFile) {
-      const allowedTypes = ['audio/mp3', 'audio/wav', 'audio/ogg'];
+      const allowedTypes = ['audio/mp3', 'audio/wav', 'audio/ogg', 'audio/mpeg']
+
       if (!allowedTypes.includes(selectedFile.type)) {
-        alert('Please select an audio file (mp3, wav, or ogg).');
-        setFile(null);
-        setFileName('');
+        e.target.value = ''
+        setFile(null)
+        setFileName('')
+        setFileTypeAlertActive(true)
       } else {
-        setFile(selectedFile);
-        setFileName(selectedFile.name);
+        setFile(selectedFile)
+        setFileName(selectedFile.name)
+        setFileTypeAlertActive(false)
       }
     }
   };
 
   return (
-    <form onFormSubmit={onFormSubmit}>
-      <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Select audio file</label>
-      <input 
-          class="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400  p-3 focus:outline-none" 
-          id="large_size" 
-          type="file"
-          onChange={onFileChange}
-      >
-      </input>
-      {fileName && <div>Selected file: {fileName}</div>}
-      <div id="fileName" class="mt-1 text-sm text-gray-600">No file selected</div>
-      <button 
-        class="mt-3 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-        type='submit'
-      >
-        Transcribe/Summarize
-      </button>
-    </form>
+    <div>
+      {
+        fileTypeAlertActive &&
+        <FailAlert subject={'File type is not valid'} message={'Please select an audio file (mp3, wav, or ogg).'} />
+      }
+      {
+        uploadFailureAlertActive &&
+        <FailAlert subject={'File Upload Failed'} message={'Something went wrong uploading audio file.'} />
+      }
+      {
+        uploadSuccessAlertActive &&
+        <SuccessAlert subject={'File Upload Successful!'} message={'Please wait while the audio file being transcribed/summarized.'} />
+      }
+    
+      <form onSubmit={onFormSubmit}>
+        <label class="block mb-2 text-sm font-medium text-gray-900">Select audio file</label>
+        <input 
+            class="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400  p-3 focus:outline-none" 
+            id="large_size" 
+            type="file"
+            onChange={onFileChange}
+        >
+        </input>
+        {fileName && <div>Selected file: {fileName}</div>}
+        <button 
+          class="mt-3 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+          type='submit'
+        >
+          Transcribe/Summarize
+        </button>
+      </form>
+    </div>
   )
 }
