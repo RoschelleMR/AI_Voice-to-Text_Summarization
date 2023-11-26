12467fa8ebb4684d3a1f5d08a6550e32a258af91
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-11-26 01:07:21 -0500

    Frontend code for generating summary; loading screens and downloading files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,16 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import TranscriptionModal from './components/TranscriptionModal';
 import AudioInput from './components/AudioInput'
+import Summarize from './components/Summarize';
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 function App() {
   const [transcriptionData, setTranscriptionData] = useState({
-    summary: '',
     transcription: ''
   })
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [summaryState, setSummaryState] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
@@ -48,10 +49,10 @@ function App() {
           // Output Section (Hidden if there isn't a file that is currently transcribed)
           transcriptionData.transcription &&
           <section>
-            <h2>Summarized Transcription</h2>
+            <h2>Transcription</h2>
 
             <div id="summaryContainer" class="p-4 bg-white rounded shadow">
-              {transcriptionData.summary}
+              {transcriptionData.transcription}
             </div>
 
             <button 
@@ -60,7 +61,30 @@ function App() {
             >
               View Transcription
             </button>
+            <a 
+                href="http://localhost:5000/api/uploads/transcription.txt"
+                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center downSum"
+            >
+                <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                <span>Download Transcription</span>
+            </a>
           </section>
+        }
+
+      { //loads summarize button once transcription is done
+        transcriptionData.transcription && 
+        <button 
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setSummaryState(true)}
+        > 
+          Summarize
+        </button>
+      } 
+
+    {
+          // Shows Summary, uses transciption to gnerate summary
+          summaryState &&
+          <Summarize transcription = {transcriptionData.transcription}/>
         }
 
       </main>
